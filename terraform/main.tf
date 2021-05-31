1f1b6c40d425e6000015a43ca10a409a81a292fe
Michael Rotmanov <rotmanov@sipgate.de>
2021-05-31 12:03:35 +0200

    added terraform with kubernetes sample setup

---
+++ terraform/main.tf
@@ -0,0 +1,48 @@
+provider "google" {
+  project = var.project_name
+  region = var.default_region
+}
+
+resource "google_service_account" "k8s_service_account" {
+  account_id   = "k8s-builder"
+  display_name = "Service Account for K8S"
+}
+
+resource "google_project_service" "project" {
+  project = var.project_name
+  service = "container.googleapis.com"
+}
+
+module "service_accounts" {
+  source        = "terraform-google-modules/service-accounts/google"
+  version       = "~> 3.0"
+  project_id    = var.project_name
+  prefix        = "test-sa"
+  names         = ["k8s-builder"]
+  project_roles = [
+    "${var.project_name}=>roles/container.clusterAdmin",
+    "${var.project_name}=>roles/container.developer",
+    "${var.project_name}=>roles/iam.serviceAccountUser",
+  ]
+}
+
+resource "google_container_cluster" "test-cluster" {
+  name = "test-cluster"
+  location = "europe-west1-c"
+  initial_node_count = 3
+  node_config {
+    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
+    service_account = module.service_accounts.email
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
+    labels = {
+      foo = "bar"
+    }
+    tags = ["foo", "bar"]
+  }
+  timeouts {
+    create = "30m"
+    update = "40m"
+  }
+}
